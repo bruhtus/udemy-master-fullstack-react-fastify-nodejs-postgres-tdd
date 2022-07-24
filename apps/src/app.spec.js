7ce8e0ee5dabaaed99679076955a9ff8fdd7ea7e
bruhtus <hibruhtus@gmail.com>
2022-07-24 09:49:01 +0700

    test(temp-route): add test case for post and get

--- apps/src/app.spec.js
+++ apps/src/app.spec.js
@@ -1,22 +1,41 @@
 const build = require('./app');
 
-describe('root route', () => {
+describe('app route', () => {
   let app;
 
-  beforeEach(() => {
+  beforeAll(() => {
     app = build();
   });
 
-  afterEach(() => {
-    app.close();
+  afterAll(async () => {
+    await app.close();
   });
 
   it('should return 200 when root route get called', async () => {
     const getResponse = await app.inject({
+      method: 'GET',
       url: '/',
     });
 
     expect(getResponse.json()).toEqual({ hello: 'world' });
     expect(getResponse.statusCode).toBe(200);
+  });
+
+  it('should return id when temp post and get route called', async () => {
+    const postResponse = await app.inject({
+      method: 'POST',
+      url: '/api/v1/test',
+      payload: { title: 'test title' },
+    });
+
+    expect(postResponse.json().id).toBeDefined();
+    expect(postResponse.statusCode).toBe(201);
+
+    const getManyResponse = await app.inject({
+      method: 'GET',
+      url: '/api/v1/test',
+    });
+
+    expect(getManyResponse.statusCode).toBe(200);
   });
 });
