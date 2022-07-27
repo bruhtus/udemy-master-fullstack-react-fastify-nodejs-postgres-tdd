5e86b62b4dae6dd207da0f93fd4615fac6f86c23
bruhtus <hibruhtus@gmail.com>
2022-07-27 09:44:29 +0700

    feat(database-plugin): add connection options to pg-promise
    note:
    so that we don't need to force exit jest when running the test. still
    not sure what is the downside of this tho.

--- apps/src/plugin/database.js
+++ apps/src/plugin/database.js
@@ -4,7 +4,10 @@ const applyMigration = require('./helper/migration');
 const config = require('../config');
 
 async function db(fastify, options, next) {
-  const dbConnection = pgp(config.database_uri);
+  const dbConnection = pgp({
+    connectionString: config.database_uri,
+    allowExitOnIdle: true,
+  });
 
   // attach our db connection to fastify with `db` name.
   // the usage: fastify.db
