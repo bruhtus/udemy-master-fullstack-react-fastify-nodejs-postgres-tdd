f9023d9875f3c984f28fd44c4b84e0f09ef46049
bruhtus <hibruhtus@gmail.com>
2022-07-28 11:33:25 +0700

    test(user-handler): add unit test for post and get by id function

---
+++ apps/src/handler/user.handler.spec.js
@@ -0,0 +1,99 @@
+const userHandler = require('./user.handler');
+const userDao = require('../data-access-object/user.dao');
+const { v4: uuidv4 } = require('uuid');
+
+// Note:
+// jest.mock return an object instead of a function.
+// meanwhile .mockImplementation() return a function.
+// console log userDao to check the result of jest mock.
+// jest.mock('../data-access-object/user.dao', () => {
+//   return {
+//     createUser: jest.fn(),
+//     getUserById: jest.fn(),
+//   };
+// });
+
+jest.mock('../data-access-object/user.dao');
+
+describe('user handler', () => {
+  const createUser = jest.fn();
+  const getUserById = jest.fn();
+
+  userDao.mockImplementation(() => {
+    return {
+      createUser,
+      getUserById,
+    };
+  });
+
+  const handler = userHandler({});
+
+  const user = {
+    first_name: 'The',
+    last_name: 'Primeagen',
+    password: 'beat_me_daddy',
+    email: 'bunspreader@dox-me-daddy.com',
+  };
+
+  const uuid = uuidv4();
+
+  it('should be able to create user', async () => {
+    createUser.mockReturnValueOnce(uuid);
+
+    const userId = await handler.createUserHandler(user);
+
+    expect(createUser).toBeCalledWith(user);
+    expect(userId).toBe(uuid);
+  });
+
+  it('should be able to get user by id', async () => {
+    const response = {
+      ...user,
+      id: uuid,
+    };
+
+    getUserById.mockReturnValueOnce(response);
+
+    const userData = await handler.getUserByIdHandler(uuid);
+
+    expect(getUserById).toBeCalledTimes(1);
+    expect(userData).toMatchObject({
+      id: response.id,
+      name: `${response.first_name} ${response.last_name}`,
+      email: user.email,
+    });
+  });
+
+  it('should be able to return the correct name when get by id', async () => {
+    const responseWithMiddleName = {
+      ...user,
+      id: uuid,
+      middle_name: 'Yayayaya',
+    };
+
+    getUserById.mockReturnValueOnce(responseWithMiddleName);
+
+    const userWithMiddleName = await handler.getUserByIdHandler(uuid);
+
+    expect(getUserById).toBeCalled();
+    expect(userWithMiddleName).toMatchObject({
+      id: uuid,
+      name: `${responseWithMiddleName.first_name} ${responseWithMiddleName.middle_name} ${responseWithMiddleName.last_name}`,
+      email: user.email,
+    });
+
+    // Ref: https://stackoverflow.com/a/53753294
+    const { last_name: _, ...responseWithoutLastName } = responseWithMiddleName;
+
+    getUserById.mockReturnValueOnce(responseWithoutLastName);
+
+    const userWithoutLastName = await handler.getUserByIdHandler(uuid);
+
+    expect(getUserById).toBeCalled();
+    expect(userWithoutLastName).toMatchObject({
+      id: uuid,
+      name: `${responseWithoutLastName.first_name} ${responseWithoutLastName.middle_name}`,
+      email: user.email,
+    });
+  });
+});
