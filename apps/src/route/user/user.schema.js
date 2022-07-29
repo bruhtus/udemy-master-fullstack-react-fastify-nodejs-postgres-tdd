7a294c3282f8f9887a33ea3fc594ea66a9a37f05
bruhtus <hibruhtus@gmail.com>
2022-07-29 07:01:14 +0700

    feat(user-route): add params and response get by id schema

--- apps/src/route/user/user.schema.js
+++ apps/src/route/user/user.schema.js
@@ -53,7 +53,57 @@ const postResponseSchema = {
   },
 };
 
+const paramsSchema = {
+  type: 'object',
+  properties: {
+    id: {
+      type: 'string',
+      format: 'uuid',
+    },
+  },
+};
+
+const getByIdResponseSchema = {
+  200: {
+    type: 'object',
+    properties: {
+      data: {
+        type: 'object',
+        properties: {
+          id: {
+            type: 'string',
+            format: 'uuid',
+          },
+          name: {
+            type: 'string',
+          },
+          email: {
+            type: 'string',
+            format: 'email',
+          },
+        },
+      },
+    },
+  },
+  400: {
+    type: 'object',
+    properties: {
+      statusCode: {
+        type: 'integer',
+      },
+      error: {
+        type: 'string',
+      },
+      message: {
+        type: 'string',
+      },
+    },
+  },
+};
+
 module.exports = {
+  getByIdResponseSchema,
   postRequestSchema,
   postResponseSchema,
+  paramsSchema,
 };
