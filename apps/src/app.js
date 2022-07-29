94736514eaa14cebd9078d25425c5447f2f0bd07
bruhtus <hibruhtus@gmail.com>
2022-07-29 08:18:14 +0700

    feat(app): register user route to fastify

--- apps/src/app.js
+++ apps/src/app.js
@@ -3,6 +3,7 @@ const db = require('./plugin/database');
 const rootRoute = require('./route/root-route');
 const testRoute = require('./route/temp-test-route');
 const swaggerPlugin = require('./plugin/swagger');
+const userRoute = require('./route/user');
 
 function build(opts = {}) {
   const app = fastify(opts);
@@ -12,8 +13,9 @@ function build(opts = {}) {
   app.register(swaggerPlugin);
 
   // register route.
-  app.register(testRoute, { prefix: 'api/v1/test' });
   app.register(rootRoute);
+  app.register(testRoute, { prefix: 'api/v1/test' });
+  app.register(userRoute, { prefix: 'api/v1/user' });
 
   return app;
 }
