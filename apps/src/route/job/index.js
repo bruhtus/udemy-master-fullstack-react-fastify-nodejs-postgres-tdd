cb0fa3488d3b707c1793907a0074a1d5e2c4eeb4
bruhtus <hibruhtus@gmail.com>
2022-07-31 09:14:06 +0700

    feat(job-route): add job route

---
+++ apps/src/route/job/index.js
@@ -0,0 +1,59 @@
+const luxon = require('luxon');
+const jobHandler = require('../../handler/job.handler');
+const schema = require('./job.schema');
+
+async function jobRoute(fastify) {
+  fastify.post(
+    '/',
+    {
+      schema: {
+        tags: ['Job'],
+        body: schema.postRequestSchema,
+      },
+      preHandler: (request, reply, done) => {
+        const { expired_at } = request.body;
+        const today = luxon.DateTime.now().toFormat('yyyy-MM-dd');
+
+        if (expired_at <= today) {
+          return reply
+            .code(400)
+            .send({ message: 'expired date must be at least tomorrow date' });
+        }
+
+        done();
+      },
+    },
+    async (request, reply) => {
+      try {
+        const body = request.body;
+
+        const id = await jobHandler.createJobHandler(fastify, body);
+        return reply.code(201).send({
+          data: { id },
+        });
+      } catch (error) {
+        return reply.code(400).send(error);
+      }
+    }
+  );
+
+  fastify.get(
+    '/',
+    {
+      schema: {
+        tags: ['Job'],
+        querystring: schema.querySchema,
+      },
+    },
+    async (request, reply) => {
+      const { limit, offset } = request.query;
+
+      const jobs = await jobHandler.getAllJobsHandler(fastify, limit, offset);
+      return reply.code(200).send({
+        data: jobs,
+      });
+    }
+  );
+}
+
+module.exports = jobRoute;
