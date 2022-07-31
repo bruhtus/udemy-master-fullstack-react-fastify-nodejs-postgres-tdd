02a01b8b3a554136e2c66bf2470866b018a07b3c
bruhtus <hibruhtus@gmail.com>
2022-07-31 09:14:19 +0700

    test(app): add integration test for job route

--- apps/src/app.spec.js
+++ apps/src/app.spec.js
@@ -1,6 +1,11 @@
+const luxon = require('luxon');
+const userRepository = require('./data-access-object/user.dao');
 const build = require('./app');
 
 describe('app route', () => {
+  let app;
+  let userId;
+
   const user = {
     first_name: 'The',
     last_name: 'Primeagen',
@@ -8,14 +13,30 @@ describe('app route', () => {
     email: 'bunspreader@dox-me-daddy.com',
   };
 
-  let app;
+  async function getUserId() {
+    const user = {
+      first_name: 'yalord',
+      last_name: 'saveme',
+      password: 'bentarlagikerja',
+      email: 'email@jobs.wtf',
+    };
 
-  beforeAll(() => {
+    const { createUser } = userRepository(app.db);
+    const id = await createUser(user);
+
+    return id;
+  }
+
+  beforeAll(async () => {
     app = build();
+
+    await app.ready();
+    userId = await getUserId();
   });
 
   afterAll(async () => {
     await app.close();
+    await app.db.query(`DELETE FROM jobs_table WHERE user_id = '${userId}'`);
   });
 
   it('should return 200 when root route get called', async () => {
@@ -78,5 +99,40 @@ describe('app route', () => {
 
     expect(getByIdResponse.json().data.id).toBe(id);
     expect(getByIdResponse.statusCode).toBe(200);
+  });
+
+  it('should be able to create job', async () => {
+    const tomorrow = luxon.DateTime.now()
+      .plus({ days: 1 })
+      .toFormat('yyyy-MM-dd');
+
+    const job = {
+      title: 'test title',
+      description: 'test description',
+      skills: 'test skills',
+      min_budget: 69,
+      max_budget: 69420,
+      expired_at: tomorrow,
+      user_id: userId,
+    };
+
+    const postResponse = await app.inject({
+      method: 'POST',
+      url: 'api/v1/job',
+      payload: job,
+    });
+
+    expect(postResponse.json().data.id).toBeDefined();
+    expect(postResponse.statusCode).toBe(201);
+  });
+
+  it('should be able to get jobs list', async () => {
+    const getManyResponse = await app.inject({
+      method: 'GET',
+      url: 'api/v1/job',
+      query: { limit: 1, offset: 0 },
+    });
+
+    expect(getManyResponse.statusCode).toBe(200);
   });
 });
